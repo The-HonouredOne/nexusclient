666c8b47acb6e1d9237cb866254b2e406daf65a1
The-HonouredOne <surendrabairwa852@gmail.com>
2025-11-25 11:53:53 +0530

    third

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,11 +40,11 @@ const Navbar = () => {
         </div>
 
     
-        <button className="md:hidden text-white hover:text-yellow-400 transition-colors duration-300">
+        {/* <button className="md:hidden text-white hover:text-yellow-400 transition-colors duration-300">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-        </button>
+        </button> */}
       </div>
     </nav>
   )
